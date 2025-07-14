44f0ae78c16825a12ec73b0804ed1ccb75b9345d
Laura-ZP <laura.zp07@gmail.com>
2025-07-14 16:02:23 +0330

    Add IdentityService to Program

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99fa9cf59814e7dc29352e1059956b8d58cfc22e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add406ee71d94561dc31af42e4b41d609db28997")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
