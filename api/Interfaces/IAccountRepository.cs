1b6e1005045391fb2c16df237ff46bd23bce8ea0
Laura-ZP <laura.zp07@gmail.com>
2026-01-02 00:02:25 +0330

    add reload logged in user

--- api/Interfaces/IAccountRepository.cs
+++ api/Interfaces/IAccountRepository.cs
@@ -6,4 +6,6 @@ public interface IAccountRepository
     public Task<LoggedInDto?> ClientRegisterAsync(ClientRegisterDto userInput, CancellationToken cancellationToken);
     public Task<LoggedInDto?> LoginAsync(LoginDto userInput, CancellationToken cancellationToken);
     public Task<DeleteResult?> DeleteByIdAsync(string userId, CancellationToken cancellationToken);
+    public Task<LoggedInDto?> ReloadLoggedInUserAsync(string userId, string token, CancellationToken cancellationToken);
+
 }
